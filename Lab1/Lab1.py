8990c80a4b412cb842aec4f64f77441d9b68ffb5
simatiz <simtchyk@gmail.com>
2021-09-15 13:19:03 +0300

    first lab

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -1,12 +1,19 @@
-x1 = double(input())
-y1 = double(input())
-x2 = double(input())
-y2 = double(input())
-x3 = double(input())
-y3 = double(input())
-a = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
-b = sqrt((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1))
-c = sqrt((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2))
-res1 = (acos((b * b + c * c - a * a) / (b * c * 2)) * 180) / math.pi
-res2 = (acos((a * a + c * c - b * b) / (a * c * 2)) * 180) / math.pi
-res3 = (acos((b * b + a * a - c * c) / (b * a * 2)) * 180) / math.pi
+import math
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+x3 = int(input())
+y3 = int(input())
+a = math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
+b = math.sqrt((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1))
+c = math.sqrt((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2))
+res1 = (math.acos((b * b + c * c - a * a) / (b * c * 2)) * 180) / math.pi
+res2 = (math.acos((a * a + c * c - b * b) / (a * c * 2)) * 180) / math.pi
+res3 = (math.acos((b * b + a * a - c * c) / (b * a * 2)) * 180) / math.pi
+print(a)
+print(b)
+print(c)
+print(res1)
+print(res2)
+print(res3)
